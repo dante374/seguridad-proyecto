a605e433850f6d27b4a83519e3b365a584bfb826
Fac-cbtti <facundococobotti@gmail.com>
2026-05-05 15:33:23 -0300

    Nav Update (No terminado)

--- src/componentes/Layout.jsx
+++ src/componentes/Layout.jsx
@@ -7,22 +7,29 @@ import Recursos from './Recursos'
 import Inicio from './Inicio'
 import Chat from './Chat'
 import Contacto from './Contacto'
-
+import { FaRegMoon } from "react-icons/fa";
 
 function Layout (){
     const navigate = useNavigate()
     return(
      <>
       <nav className="Nav">
-        <Link className='titulo-nav' to="/"><h1>Navega <span>Seguro</span></h1></Link>
+        <div className='titulo-nav' to="/"><h1>Navega<span>Seguro</span></h1></div>
         <div className="listado">
             <ul className="links">
-                <li><Link to="/Riesgos">Riesgos</Link></li>
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="/Riesgos">Riesgos de internet</Link></li>
                 <li><Link to="/Consejos">Consejos</Link></li>
                 <li><Link to="/Padres">Padres y docentes</Link></li>
                 <li><Link to="/Recursos">Recursos</Link></li>
-                <li><Link to="/Contacto">Contacto</Link></li>
             </ul>
+        </div>
+        <div>
+            <ul className='izquierda'>
+                <li><FaRegMoon /></li>
+                <li id='contacto-boton'><Link to="/Contacto">Contacto</Link></li>
+            </ul>
+            
         </div>
       </nav>
       <Outlet/>
