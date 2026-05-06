e06fd7a4db1baac592ed89650f216a2edc11ebb1
Fac-cbtti <facundococobotti@gmail.com>
2026-05-06 14:48:49 -0300

    Navbar terminado

--- src/componentes/Layout.jsx
+++ src/componentes/Layout.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../Styles/Layout.css'
 import Riesgos from './Riesgos'
 import Padres from './Padres'
@@ -14,20 +15,20 @@ function Layout (){
     return(
      <>
       <nav className="Nav">
-        <div className='titulo-nav' to="/"><h1>Navega<span>Seguro</span></h1></div>
+        <NavLink className='titulo-nav' to="/"><h1>Navegar<span>Seguro</span></h1></NavLink>
         <div className="listado">
             <ul className="links">
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/Riesgos">Riesgos de internet</Link></li>
-                <li><Link to="/Consejos">Consejos</Link></li>
-                <li><Link to="/Padres">Padres y docentes</Link></li>
-                <li><Link to="/Recursos">Recursos</Link></li>
+                <li><NavLink to="/">Inicio</NavLink></li>
+                <li><NavLink to="/Riesgos">Riesgos de internet</NavLink></li>
+                <li><NavLink to="/Consejos">Consejos</NavLink></li>
+                <li><NavLink to="/Padres">Padres y docentes</NavLink></li>
+                <li><NavLink to="/Recursos">Recursos</NavLink></li>
             </ul>
         </div>
         <div>
             <ul className='izquierda'>
                 <li><FaRegMoon /></li>
-                <li id='contacto-boton'><Link to="/Contacto">Contacto</Link></li>
+                <li id='contacto-boton'><NavLink to="/Contacto">Contacto</NavLink></li>
             </ul>
             
         </div>
